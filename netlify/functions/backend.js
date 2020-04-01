051e4bdc43240c79188103002a89a7df53de28a6
Evan Richards <erichards18@georgefox.edu>
2020-03-31 17:13:03 -1000

    backend yay?

--- netlify/functions/backend.js
+++ netlify/functions/backend.js
@@ -1,8 +1,20 @@
 // console.log("Returning " + API_MAP);
 
-exports.handler = function(event, context, callback) {
-    callback(null, {
-      statusCode: 200,
-      body: `Hello ${process.env.API_MAP}`
-    });
-  };
+exports.handler = async (event, context, callback) => {
+    const pass = (body) => {callback(null, {statusCode: 200, body: JSON.stringify(body)})}
+  
+    try {
+    let response = await fetch(`https://maps.googleapis.com/maps/api/js?key=${process.env.API_TOKEN}&callback=initMap`)
+    .then((response) => {
+        let data = response.json()
+        pass(data)
+      }
+    )
+   } catch(err) {
+       let error = {
+         statusCode: err.statusCode || 500,
+         body: JSON.stringify({error: err.message})
+   }
+    await pass(error)
+   }
+  }
