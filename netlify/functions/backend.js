bf2ecb6b824d8b0a1cc9f3bde99e09d24a512e87
Evan Richards <erichards18@georgefox.edu>
2020-03-31 16:15:54 -1000

    toml update

--- netlify/functions/backend.js
+++ netlify/functions/backend.js
@@ -1,7 +1,7 @@
 export function handler(event, context, callback) {
-    console.log("Returned " + API_MAP);
+    console.log("Returned " + "API_MAP");
     callback(null, {
     statusCode: 200,
-    body: API_MAP
+    body: "API_MAP"
     });
 }
