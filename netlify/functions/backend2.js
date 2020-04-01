43b0fbe5c26dc4f093a6e5bbf8f359eda3d903ab
Evan Richards <erichards18@georgefox.edu>
2020-03-31 17:02:13 -1000

    try2

--- netlify/functions/backend2.js
+++ netlify/functions/backend2.js
@@ -10,14 +10,27 @@
 const fetch = require("node-fetch");
 
 
+  // exports.handler = async (event, context, callback) => {
+  //   const pass = (body) => {callback(null, {statusCode: 200, body: JSON.stringify(body)})}
+  
+  //   try {
+  //   let response = await fetch(`https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_API}&callback=initMap`)
+  //    let data = await response.json()
+  //    await pass(data)
+  //  } catch(err) {
+  //      let error = {
+  //        statusCode: err.statusCode || 500,
+  //        body: JSON.stringify({error: err.message})
+  //  }
+  //   await pass(error)
+  //  }
+  // }
+  
   exports.handler = async (event, context, callback) => {
     const pass = (body) => {callback(null, {statusCode: 200, body: JSON.stringify(body)})}
   
     try {
-      let response = await fetch(`https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_API}&callback=initMap`, 
-    {
-     body: event.body
-    })
+    let response = await fetch(`https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_API}&callback=initMap`)
      let data = await response.json()
      await pass(data)
    } catch(err) {
