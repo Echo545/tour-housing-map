069ee98974cf4b78c48729e2d3bfc78957acdcc3
Evan Richards <erichards18@georgefox.edu>
2020-04-01 16:13:55 -1000

    another safari test

--- assets/js/housing.js
+++ assets/js/housing.js
@@ -1,117 +1,115 @@
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 class Housing {
-
-    constructor(address, contactName, contactInfo, year, notes){
-
-        if (typeof address === "undefined"){
-            address = "421 8th St, Huntington Beach, CA 92648";
-        }
-
-        if(typeof contactName === "undefined"){
-            contactName = "Brian Brennt";
-        }
-
-        if(typeof contactInfo === "undefined"){
-            contactInfo = "123-998-1234";
-        }
-
-        if(typeof year === "undefined"){
-            year = "2018";
-        }
-
-        if(typeof notes === "undefined"){
-            notes = "This house is the best, it's a big big house with lots of rooms";
-        }
-
-        this.address = address;
-        this.contactName = contactName;
-        this.contactInfo = contactInfo;
-        this.year = year;
-        this.notes = notes;
-
-        // Marker and location are set once the address has been marked on the map
-        this.marker = "";
-        this.location = "";
-
-        // set ID
-        this.id = Housing.idCounter;
-        Housing.idCounter++;
+  constructor(address, contactName, contactInfo, year, notes) {
+    if (typeof address === "undefined") {
+      address = "421 8th St, Huntington Beach, CA 92648";
     }
 
-    // this gives every housing object a unique ID
-    static idCounter = 0;
-
-
-    get location(){
-        return this._location;
+    if (typeof contactName === "undefined") {
+      contactName = "Brian Brennt";
     }
 
-    get address(){
-        return this._address;
+    if (typeof contactInfo === "undefined") {
+      contactInfo = "123-998-1234";
     }
 
-    get contactName(){
-        return this._contactName;
+    if (typeof year === "undefined") {
+      year = "2018";
     }
 
-    get contactInfo(){
-        return this._contactInfo;
+    if (typeof notes === "undefined") {
+      notes = "This house is the best, it's a big big house with lots of rooms";
     }
 
-    get year(){
-        return this._year;
-    }
+    this.address = address;
+    this.contactName = contactName;
+    this.contactInfo = contactInfo;
+    this.year = year;
+    this.notes = notes; // Marker and location are set once the address has been marked on the map
 
-    get notes(){
-        return this._notes;
-    }
+    this.marker = "";
+    this.location = ""; // set ID
 
-    get housingName(){
-        return `${this.contactName}'s House`;
-    }
+    this.id = Housing.idCounter;
+    Housing.idCounter++;
+  } // this gives every housing object a unique ID
 
-    get marker(){
-        return this._marker;
-    }
 
-    get id(){
-        return this._id;
-    }
+  get location() {
+    return this._location;
+  }
 
-    set location(loc){
-        this._location = loc;
-    }
+  get address() {
+    return this._address;
+  }
 
-    set address(addr){
-        this._address = addr;
-    }
+  get contactName() {
+    return this._contactName;
+  }
 
-    set contactName(ctcName){
-        this._contactName = ctcName;
-    }
+  get contactInfo() {
+    return this._contactInfo;
+  }
 
-    set contactInfo(ctcInfo){
-        this._contactInfo = ctcInfo;
-    }
+  get year() {
+    return this._year;
+  }
 
-    set year(yr){
-        this._year = yr;
-    }
+  get notes() {
+    return this._notes;
+  }
 
-    set notes(nts){
-        this._notes = nts;
-    }
+  get housingName() {
+    return `${this.contactName}'s House`;
+  }
 
-    set marker(mkr){
-        this._marker = mkr;
-    }
+  get marker() {
+    return this._marker;
+  }
 
-    set id(i){
-        this._id = i;
-    }
+  get id() {
+    return this._id;
+  }
 
-    toString(){
-        return `id: ${this.id}\nname: ${this.housingName}\nlocation: ${this.location}\naddress: ${this.address}\ncontact name: ${this.contactName}\ncontact info: ${this.contactInfo}\nyear: ${this.year}\nnotes: ${this.notes}`;
-    }
+  set location(loc) {
+    this._location = loc;
+  }
+
+  set address(addr) {
+    this._address = addr;
+  }
+
+  set contactName(ctcName) {
+    this._contactName = ctcName;
+  }
+
+  set contactInfo(ctcInfo) {
+    this._contactInfo = ctcInfo;
+  }
+
+  set year(yr) {
+    this._year = yr;
+  }
+
+  set notes(nts) {
+    this._notes = nts;
+  }
+
+  set marker(mkr) {
+    this._marker = mkr;
+  }
+
+  set id(i) {
+    this._id = i;
+  }
+
+  toString() {
+    return `id: ${this.id}\nname: ${this.housingName}\nlocation: ${this.location}\naddress: ${this.address}\ncontact name: ${this.contactName}\ncontact info: ${this.contactInfo}\nyear: ${this.year}\nnotes: ${this.notes}`;
+  }
+
 }
+
+_defineProperty(Housing, "idCounter", 0);
 
 export default Housing;
